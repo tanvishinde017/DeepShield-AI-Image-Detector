a3d601fff4df3ec7de63cd1a841c935ed22e828b
Tanvishinde017 <tanvishinde1560@gmail.com>
2026-02-23 17:25:16 +0530

    updated req

--- backend/app.py
+++ backend/app.py
@@ -122,5 +122,5 @@ def predict_image():
 # =========================
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
