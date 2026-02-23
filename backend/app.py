166009da45137c2b23b852d73bf085a42bdfe2a8
Tanvishinde017 <tanvishinde1560@gmail.com>
2026-02-23 22:30:29 +0530

    FIXED PORT case and dynamic binding

--- backend/app.py
+++ backend/app.py
@@ -122,5 +122,6 @@ def predict_image():
 # =========================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port =int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
